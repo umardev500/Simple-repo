dfe5db5e5e9dfffb8f3c09fa109edea4d5c2120b
umardev500 <umar.dev500@gmail.com>
2023-10-22 09:15:58 +0700

    make footer text center

--- src/components/organisms/footer/Footer.tsx
+++ src/components/organisms/footer/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className="mt-6 flex bg-gray-100 items-center justify-center px-4 py-5 font-normal text-gray-600">
+    <footer className="mt-6 text-center flex bg-gray-100 items-center justify-center px-4 py-5 font-normal text-gray-600">
       © 2023 Pixel8Labs. All rights reserved.
     </footer>
   );
