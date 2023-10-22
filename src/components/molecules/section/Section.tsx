c0cf658389b9288d2c2b2d521c96732f4d8c9aeb
umardev500 <umar.dev500@gmail.com>
2023-10-22 07:48:06 +0700

    parse repo length

--- src/components/molecules/section/Section.tsx
+++ src/components/molecules/section/Section.tsx
@@ -1,10 +1,14 @@
 import { BadgeMd, Heading } from "@/components/atoms";
+import { AppContext, type AppContextType } from "@/context/AppContext";
+import { useContext } from "react";
 
 export const Section = () => {
+  const ctx = useContext(AppContext) as AppContextType;
+
   return (
     <div className="flex items-center gap-3 h-20">
       <Heading text="Repository" />
-      <BadgeMd text="6" />
+      <BadgeMd text={ctx.repos?.length.toString() ?? "0"} />
     </div>
   );
 };
